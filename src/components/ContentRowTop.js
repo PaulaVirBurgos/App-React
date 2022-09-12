20f61c0cad7b07ea8e67ca8b7dcc57eab50b3594
Paula Virginia Burgos <paulavburgos@gmail.com>
2022-09-12 15:48:10 -0300

    app react terminado

---
+++ src/components/ContentRowTop.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ContentRowMovies from './ContentRowMovies';
+import GenresInDb from './GenresInDb';
+import LastMoviesInDb from './LastMoviesInDb';
+
+const ContentRowTop = () => {
+    return (
+        <div className="container-fluid">
+            <div className="d-sm-flex align-items-center justify-content-between mb-4">
+                <h1 className="h3 mb-0 text-gray-800">App Dashboard</h1>
+            </div>
+
+
+            <ContentRowMovies />
+
+
+
+
+            <div className="row">
+
+                <LastMoviesInDb />
+                <GenresInDb />
+            </div>
+        </div>
+    );
+}
+
+export default ContentRowTop;
